6a696463a09fa86ddc608ad0fe06c8541859e058
Nam Quang <mrquangtn@gmail.com>
2016-12-27 15:05:43 +0700

    - Push code to ver 1.0.0

---
+++ ExportReport/Program.cs
@@ -0,0 +1,20 @@
+﻿using ScalesData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportReport
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Launcher launcher = new Launcher();
+            launcher.showView(true);
+        }
+    }
+}
